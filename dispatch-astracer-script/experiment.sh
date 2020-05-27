025df130cbfa23328acbd1c69a5b80aec1c4c774
yangsoon <yangsoonlx@gmail.com>
2020-05-27 08:57:26 +0800

    添加插桩脚本

--- dispatch-astracer-script/experiment.sh
+++
@@ -1,7 +0,0 @@
-#python experiment.py wordcount $1 10
-python experiment.py sort $1 10
-python experiment.py terasort $1 10
-python experiment.py dfsioe $1 10
-python experiment.py pagerank $1 10
-python experiment.py kmeans $1 10
-
